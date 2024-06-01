7ef1163ddd6392e6377195f3d5346e6d9682a96c
hildhild <huy.ledinh@hcmut.edu.vn>
2024-06-02 00:22:43 +0700

    add nativewind

---
+++ SBMS/app/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+
+
+export default function Login() {
+  return (
+    <View >
+      <Text className="p-[120px]">Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+
+
