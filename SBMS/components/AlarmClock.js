afc75ceb5bba0770bfbd5e9600a857bfb82f1514
sitrongtrang <sitrongtrang@gmail.com>
2024-06-11 18:15:57 +0700

    finish Alarm fe

--- SBMS/components/AlarmClock.js
+++ SBMS/components/AlarmClock.js
@@ -1,25 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Switch, TextInput, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Switch, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; 
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons/faAngleRight';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const AlarmClock = ({ id, hour, minute, active, remove }) => {
+const AlarmClock = ({ id, hour, minute, remove }) => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    const handleRemove = () => {
+        
+    };
+
     return (
         <View style={styles.container}>
-            <View style={styles.section}>
-                <Image></Image>
-                <Text style={styles.title}>{`${formatNumber(hour)}:${formatNumber(minute)}`}</Text>
-                <Switch thumbColor={"#45b6fe"}/>
-            </View>
-            <View style={styles.section}>
-                <Text>Wake up</Text>
+            <View style={styles.mainSection}>
+                <View style={styles.leftSection}>
+                    {remove ? (
+                        <TouchableOpacity onPress={handleRemove} style={styles.trashButton}>
+                            <Ionicons name="trash-outline" size={24} color="red" />
+                        </TouchableOpacity>
+                    ) : (
+                        <Icon style={styles.trashButton} name="clock-o" size={24} color="dodgerblue" />
+                    )}
+                    <View style={styles.textSection}>
+                        <Text style={styles.title}>{hour} : {minute}</Text>
+                        <Text style={styles.subtitle}>Wake up</Text>
+                    </View>
+                </View>
+                {remove ?  (
+                    <TouchableOpacity
+                        style={styles.arrowButton}
+                        onPress={() => navigation.goBack()}
+                    >
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </TouchableOpacity>
+                    ) : (<Switch
+                        thumbColor={"dodgerblue"}
+                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        onValueChange={toggleSwitch}
+                        value={isEnabled}
+                    />)
+                }
             </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: { flex: 1, flexDirection: 'column', paddingHorizontal: 20, marginBottom: 5, paddingBottom: 10, padding: 5, borderBottomColor: '#e8e8e8', borderBottomWidth: 2, borderRadius: 10 },
-    section: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginVertical: 2 },
-    title: { fontSize: 32, textAlign: 'center' },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingHorizontal: 20,
+        marginBottom: 5,
+        paddingBottom: 10,
+        padding: 5,
+        borderBottomColor: '#e8e8e8',
+        borderBottomWidth: 2,
+        borderRadius: 10,
+    },
+    mainSection: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginVertical: 2,
+    },
+    leftSection: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    textSection: {
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 32,
+        textAlign: 'center',
+    },
+    subtitle: {
+        fontSize: 16,
+        textAlign: 'center',
+        color: 'gray',
+    },
+    trashButton: {
+        marginRight: 30,
+    },
+    arrowButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 40,
+        height: 40,
+        backgroundColor: '#F7F7F9',
+        borderRadius: 20,
+    },
 });
 
 export default AlarmClock;
--- SBMS/views/AlarmScreen.js
+++ SBMS/views/AlarmScreen.js
@@ -1,16 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Switch, TextInput, StyleSheet, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
 import AlarmClock from '../components/AlarmClock';
+import { Ionicons} from '@expo/vector-icons';
 //import axios from 'axios';
 
 const AlarmScreen = () => {
-    const [alarms, setAlarms] = useState([]);
+    const [editMode, setEditMode] = useState(false);
+    const [alarms, setAlarms] = useState([
+        {id: 1, hour: "06", minute: "30"},
+        {id: 2, hour: "06", minute: "30"},
+        {id: 3, hour: "06", minute: "30"},
+    ]);
 
     return (
         <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => {setEditMode(previousState => !previousState);}}>
+                {editMode ? (<Text style={styles.editButton}>Done</Text>) :
+                (<Text style={styles.editButton}>Edit</Text>)}
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.addButton}>
+                <Ionicons name="add-outline" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
             <ScrollView style={{ flexGrow: 1, width: '100%', height: '100%' }}>
-                <View>
-                    {alarms.map((alarm, i) => <AlarmClock key={alarm.id} id={alarm.id} hour={alarm.hour} minute={alarm.minute} active={alarm.active} remove={remove} />)}
+                <View style={styles.alarmGrid}>
+                    {alarms.map((alarm, i) => <AlarmClock key={alarm.id} id={alarm.id} hour={alarm.hour} minute={alarm.minute} remove={editMode} />)}
                 </View>
             </ScrollView>
         </View>
@@ -18,11 +33,40 @@ const AlarmScreen = () => {
 };
 
 const styles = StyleSheet.create({
-    safeArea: { flex: 1 },
-    container: { flex: 1, flexDirection: 'column', alignItems: 'center', backgroundColor: '#f0f2f5' },
-    panel: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-    button: { position: 'absolute', bottom: 70, left: Dimensions.get('window').width / 2 - 110 },
-    colors: { black: '#000', white: 'white' },
+    safeArea: { 
+        flex: 1 
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 16,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+      editButton: {
+        color: 'dodgerblue',
+        fontSize: 20,
+    },
+    panel: { 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    },
+    button: { 
+        position: 'absolute', 
+        bottom: 70, 
+        left: Dimensions.get('window').width / 2 - 110 
+    },
+    colors: { 
+        black: '#000', 
+        white: 'white' 
+    },
+    alarmGrid: {
+        marginTop: 16,
+    },
 
 });
 
